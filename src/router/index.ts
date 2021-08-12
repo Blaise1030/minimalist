4fd57b4ea4eac3a08cfdb0915ef1acc9bcbe6a3b
Blaise1030 <blaisetiong1030@gmail.com>
2021-08-12 22:50:44 +0800

    Added protected routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,46 @@
 import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
-import List from "../views/List.vue";
-import Main from "../views/Main.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Main",
-    component: Main,
+    component: () => import(/* webpackChunkName: "main" */ "../views/Main.vue"),
   },
   {
     path: "/list",
     name: "list",
-    component: List,
+    component: () => import(/* webpackChunkName: "list" */ "../views/List.vue"),
   },
   {
     path: "/all",
     name: "AllList",
-    component: () => import("../views/AllList.vue"),
+    component: () =>
+      import(/* webpackChunkName: "AllList" */ "../views/AllList.vue"),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, _, next) => {
+  if (to.fullPath === "/all") {
+    if (Math.random() > 0.5) {
+      next("/");
+    }
+  }
+  if (to.fullPath === "/list") {
+    if (Math.random() > 0.5) {
+      next("/");
+    }
+  }
+  if (to.fullPath === "/") {
+    if (Math.random() > 0.5) {
+      next("/all");
+    }
+  }
+  next();
 });
 
 export default router;
